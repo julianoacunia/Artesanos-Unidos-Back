5b490fa8650a679d722e57d8371ab3e202163707
Juliano Acuña <juliano.acunia@hotmail.com>
2020-04-21 19:13:13 -0300

    Decode token

--- app/functions/index.js
+++ app/functions/index.js
@@ -14,6 +14,26 @@ function createToken(user) {
     return jwtSimple.encode(payload, config.token_key)
 }
 
+// THIS IS A TOKEN MIDDLEWARE
+function decodeToken(req, res, next) {
+    try {
+      console.log('HEADERS', req.headers)
+      const token = req.headers.authorization.replace('BEARER ', '')
+      const payload = jwtSimple.decode(token, config.token_key)
+      if (payload.exp <= moment().unix()) {
+        return res
+          .status(401)
+          .send({ status: 401, message: 'The token has expired' })
+      }
+      next()
+    } catch (err) {
+      return res
+        .status(500)
+        .send({ status: 500, message: 'Incorrect token', err })
+    }
+}
+
 module.exports = {
-    createToken
+    createToken,
+    decodeToken
 }
