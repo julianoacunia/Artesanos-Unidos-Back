6a5d109a6bfa52cf0c788993e2e1910ffbba84bc
Juliano Acuña <juliano.acunia@hotmail.com>
2020-04-21 19:18:23 -0300

    Insert user method

--- app/router/users/controller.js
+++ app/router/users/controller.js
@@ -9,3 +9,22 @@ const getAll = (req, res) => {
       res.send(users)
     })
 }
+
+// Insert user method
+const insertUser = (req, res) => {
+    const user = new User ({
+        name: req.body.name,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        password: sha256(req.body.password)
+    })
+    user.save(err => {
+        if (err) res.send({ msg: 'Cant`t save the user', error: err })
+        res.send({ msg: 'account saved' })
+    })
+}
+
+module.exports = {
+    getAll,
+    insertUser
+}
