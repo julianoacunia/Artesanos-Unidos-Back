c556ffb284b7c5dee24d6ebbe81793707d9f8ce0
Juliano Acuña <juliano.acunia@hotmail.com>
2020-04-21 19:56:30 -0300

    Add categorie in router general

--- app/router/index.js
+++ app/router/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const product = require('./products')
 const user = require('./users')
+const categorie = require('./categories')
 
 const router = express.Router()
 
 router.use('/products', product)
 router.use('/users', user)
+router.use('/categories', categorie)
 
 module.exports = router
