72ef52b7a25f32e9dfbfc205bf15ed61b236761a
Juliano Acuña <juliano.acunia@hotmail.com>
2020-04-21 19:25:09 -0300

    Add user in router general

--- app/router/index.js
+++ app/router/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const product = require('./products')
+const user = require('./users')
 
 const router = express.Router()
 
 router.use('/products', product)
+router.use('./users', user)
 
 module.exports = router
