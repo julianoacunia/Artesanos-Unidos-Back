677548f574fa4df3b159aaababf50308926cb296
Juliano Acuña <juliano.acunia@hotmail.com>
2020-04-21 17:42:46 -0300

    Create router products

---
+++ app/router/products/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const controller = require('./controller')
+
+const router = express.Router()
+const { getAll, getById, insertProduct, upsertProduct, removeProduct } = controller
+
+router.use(express.json())
+
+router.get('/', getAll)
+router.get('/:id', getById)
+router.post('/', insertProduct)
+router.put('/:id', upsertProduct)
+router.delete('/:id', removeProduct)
+
+module.exports = router
